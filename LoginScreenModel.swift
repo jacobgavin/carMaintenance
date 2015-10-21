528a75cd606d8f4367d3e5be62bf560f298ec653
Dijkstra7 <rickr.dijkd@gmail.com>
2015-10-21 18:28:00 +0200

    It’s Working!!!!
    I now need to incorporate it with the first version from Wouter. But I
    have a working pincode-pad.
    you can type a code, press backspace and when it’s correct you go to
    the next page. when the pin is incorrect it will show an error message

--- LoginScreenModel.swift
+++ LoginScreenModel.swift
@@ -8,10 +8,14 @@
 
 import Foundation
 class LoginScreenModel {
-    var pincode = "1234";
+    init(pincode: String){
+        self.pincode = pincode
+    }
+    var pincode = "";
     var pintry = ""
     var pinLabel = ""
-    func setLabel(points: Int){
+    func setLabel(){
+        let points = pintry.characters.count
         pinLabel = ""
         if points>0{
             for _ in (0..<points){
@@ -19,17 +23,37 @@ class LoginScreenModel {
             }
         }
     }
-    func getPinLabel() -> String{
+    
+    func getPinLabel() -> String //lets the model have the representation of the pincode label
+    {
         return pinLabel;
     }
-    func checkPincode(pintry: String) ->	Bool{
+    
+    func pincodeIsCorrect() ->	Bool //check of the pincode is consistent with the entered one
+    {
         return pincode == pintry;
     }
-    func addDigit(pinDigit: String) {
-        pintry = pintry + pinDigit;
-        print("pintry = \(pintry)")
-        setLabel(pintry.characters.count)
+    
+    func erasePincode(){
+        pintry = ""
+        setLabel()
     }
+    
+    
+    func addDigit(pinDigit: String) {
+        pintry = pintry + pinDigit; //add digit to the try
+        print("pintry = \(pintry)")
+        setLabel() //
+    }
+    
+    func deleteDigit(){
+        if !pintry.isEmpty{ //delete the last digit
+            let indexPinDel = pintry.endIndex.advancedBy(-1)
+            pintry = pintry.substringToIndex(indexPinDel)
+        }
+        setLabel() //update the string represent from the pincode label
+    }
+
 }
 
 
--- carMaintanence/ViewController.swift
+++ carMaintanence/ViewController.swift
@@ -12,10 +12,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var pincodeLabel: UILabel!
     var labelText = 0;
-    var LogScreen = LoginScreenModel();
+    var LogScreen = LoginScreenModel(pincode: "1234");
     @IBAction func buttonClick(sender: UIButton) {
-        let digit = sender.currentTitle!
-        LogScreen.addDigit("\(digit)");
+        let digit = sender.currentTitle! //get the value of tthe pressed number
+        LogScreen.addDigit("\(digit)"); //add it to the string of the pin
+        updatePincode()
+    }
+    
+    @IBAction func pressBackspace() {
+        LogScreen.deleteDigit() //erase a digit from the pin
+        updatePincode()
+    }
+    
+    @IBAction func pressEnter(sender: UIButton) {
+        if LogScreen.pincodeIsCorrect(){
+            goToMonteurs()
+        }
+        else{
+            pincodeLabel.text = "Wrong pincode"
+            LogScreen.erasePincode()
+        }
+        
+    }
+    
+    func goToMonteurs(){
+        performSegueWithIdentifier("Monteurs", sender: nil)
+    }
+    
+    func updatePincode(){ //update the representation of the pincode
         pincodeLabel.text = LogScreen.getPinLabel()
     }
 }
