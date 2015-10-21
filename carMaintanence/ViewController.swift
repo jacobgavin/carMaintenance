bec6bc180c8e10dd1794066f992911bbf25acb98
Wvdven <wouter.vanderven@gmail.com>
2015-10-21 16:08:48 +0200

    Fixed bugs

--- carMaintanence/ViewController.swift
+++ carMaintanence/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionViewController: UICollectionViewController	 {
 
+    
+    var Array = ["persoon1" , "persoon2" , "persoon3", "persoon4" , "persoon5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return Array.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        var cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        var Button = cell.viewWithTag(1) as! UILabel
+        Button.text = Array[indexPath.row]
 
+        return cell
+    }
+// test
 
 }
 
