ff17aee53b1050b78b66125adeedd847d7c01ee0
gavve <jacobgavin89@gmail.com>
2015-12-03 15:00:17 +0100

    The niece activities screen, the activities screen and the Foto's view are all in here and working

---
+++ carMaintenance/camera.swift
@@ -0,0 +1,59 @@
+//
+//  camera.swift
+//  carMaintenance
+//
+//  Created by vmware on 03/12/15.
+//  Copyright © 2015 vmware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CameraView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    @IBAction func chooseImageFromPhoto() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        
+        presentViewController(picker, animated: true, completion:nil)
+    }
+    
+    @IBAction func chooseImageFromCamera() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion:nil)
+    }
+   
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [NSObject : AnyObject]?) {
+        imageView.image = image
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBOutlet weak var stackImageView: UIStackView!
+    
+    @IBAction func addPhotoToStack(sender: AnyObject) {
+        let productImage:UIImageView = UIImageView(image: imageView.image)
+        productImage.contentMode = .ScaleAspectFit
+        self.stackImageView.addArrangedSubview(productImage)
+        UIView.animateWithDuration(0.25, animations: {
+            self.stackImageView.layoutIfNeeded()
+        })
+    }
+}
